755bd32a3d88bffd86bf45b4f30198a5b21a4ebe
tanishab557 <98122051+tanishab557@users.noreply.github.com>
2022-01-22 22:15:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome! Tanisha V1"
+
+@app.route('/how')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
